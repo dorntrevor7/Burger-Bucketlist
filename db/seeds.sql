b8ea603f149aeaf04027116852c19637169c87e7
Trevor Dorn <dorntrevor7@gmail.com>
2020-03-15 16:55:47 -0700

    Init refactoring

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burger_db
+
+INSERT INTO burgers (name) VALUES ('PB&J Burger');
+INSERT INTO burgers (name, devour) VALUES ('Cheese Burger', true);
+INSERT INTO burgers (name, devour) VALUES ('Hamburger', true);
